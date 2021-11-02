2071e42aa015b2d4d4593958eab4bc36de59ed1b
ricaza81 <ricaza81@gmail.com>
2021-11-02 00:59:59 -0500

    fixes

--- resources/views/landing/visor.blade.php
+++ resources/views/landing/visor.blade.php
@@ -291,7 +291,7 @@
       </div>
   <div class="container">
      <div class="row">
-        <a href="//cdn.embedly.com/widgets/media.html?src=https%3A%2F%2Fdatastudio.google.com%2Fembed%2Freporting%2Fa2b9da07-31a4-423e-ab45-bc1d00f29a1d%2Fpage%2FmM3VB%3Ffbclid%3DIwAR2XCOsj8zcLXEAEP5Ruz548A57UknlXK3Eab-DwAk9SCFwjCtbttXwhABc%26params%3D%257B%2522df9%2522%3A%2522include%2525EE%252580%2525800%2525EE%252580%252580IN%2525EE%252580%252580EmpresaDemo%2522%257D%26feature%3Doembed&dntp=1&display_name=Google+Data+Studio&url=https%3A%2F%2Fdatastudio.google.com%2Freporting%2Fa2b9da07-31a4-423e-ab45-bc1d00f29a1d%2Fpage%2FmM3VB%3Ffbclid%3DIwAR2XCOsj8zcLXEAEP5Ruz548A57UknlXK3Eab-DwAk9SCFwjCtbttXwhABc%26params%3D%257B%2522df9%2522%3A%2522include%2525EE%252580%2525800%2525EE%252580%252580IN%2525EE%252580%252580EmpresaDemo%2522%257D&image=https%3A%2F%2Fdatastudio.google.com%2Freporting%2Fa2b9da07-31a4-423e-ab45-bc1d00f29a1d%2Fpage%2FmM3VB%2Fthumbnail%3Fsz%3Dw1200-h900-p-nu%26feature%3Doembed&key=internal&type=text%2Fhtml&schema=google" width="1800" height="650" scrolling="yes" title="Google Data Studio embed" frameborder="0" allow="autoplay; fullscreen" allowfullscreen="true" style="margin-top:0px;margin-left:0px;margin-right:0px;margin-bottom:50px" class="newboton" data-wow-delay="0.0s" style="margin-top: 40px" target="_blank">Pantalla Completa
+        <a href="https://datastudio.google.com/s/g-CTCI60p_g" width="1800" height="650" scrolling="yes" title="Google Data Studio embed" frameborder="0" allow="autoplay; fullscreen" allowfullscreen="true" style="margin-top:0px;margin-left:0px;margin-right:0px;margin-bottom:50px" class="newboton" data-wow-delay="0.0s" style="margin-top: 40px" target="_blank">Pantalla Completa
         </a>
     </div>
   </div>
